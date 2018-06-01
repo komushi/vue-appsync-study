5787878646b482f862cf42708b6050a62f1611a8
Lei Xu <komushi@gmail.com>
2018-06-01 12:30:01 +0900

    minor change

--- src/appsync/index.js
+++ src/appsync/index.js
@@ -2,10 +2,7 @@ import AppSyncRouter from './router';
 import AppSyncView from './views/AppSyncView.vue';
 import { AppSyncProvider } from './vue-appsync'
 
-// export * from './components'
-
 export {
 	AppSyncProvider,
-	AppSyncRouter,
-	// AppSyncView
+	AppSyncRouter
 }
